4b9715d22b97eead9305d5235b1c2b32d11a5aa2
Mark <markradin6886@gmail.com>
2025-05-14 23:26:03 +0300

    140525a

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ function setCellContent(table, rowIndex, colIndex, content) {
 function setElement(element, table){
     setCellContent(table, element.period, element.group, `
         <div class="element ${element.type} ${(element.type === 'd' || element.type === 'f') ? 'b' : 'a'}">
+            <div class="charge">${element.charge}</div>
             <div class="letter">${element.letter}</div>
             <div class="name">${element.name}</div>
             <div class="mass">${element.mass}</div>
-            <div class="charge">${element.charge}</div>
             <div class="electronegativity">${element.electronegativity}</div>
             <div class="atomic-radius">${element.atomicRadius}</div>
             <div class="ionization-energy">${element.ionizationEnergy}</div>
@@ -59,3 +59,42 @@ function setElement(element, table){
         </div>
     `)
 }
+
+
+const supMap = {
+    '⁰': '0', '¹': '1', '²': '2', '³': '3', '⁴': '4',
+    '⁵': '5', '⁶': '6', '⁷': '7', '⁸': '8', '⁹': '9'
+};
+function parseElectronConfiguration(ec){
+    let configuration = []
+    for(let i = 0; i < ec.length; i++){
+        if(!isNaN(ec[i]-0)){
+            if(configuration.length < ec[i]-0) configuration.push([])
+            configuration[ec[i]-1].push({
+                type: NaN,
+                count: NaN
+            })
+        }else if(!Object.keys(supMap).includes(ec[i])){
+            if(typeof configuration[ec[i-1]-1] !== 'undefined'){
+                console.log(configuration[ec[i-1]-1][configuration[ec[i-1]-1].length-1])
+                configuration[ec[i-1]-1][configuration[ec[i-1]-1].length-1].type = ec[i]
+            }
+        }else{
+            count = ''
+            j = i
+            while(Object.keys(supMap).includes(ec[j])){
+                count += supMap[ec[j]]
+                j++
+            }
+            console.log(count)
+            j--
+            if(typeof configuration[ec[i-2]-1] !== 'undefined'){
+                console.log(1, configuration[ec[i-2]-1][configuration[ec[i-2]-1].length-1])
+                configuration[ec[i-2]-1][configuration[ec[i-2]-1].length-1].count = count-0
+            }
+            i = j
+        }
+    }
+    return configuration
+};
+console.log(parseElectronConfiguration('1s²2s²2p⁶3s²3p⁶'))
